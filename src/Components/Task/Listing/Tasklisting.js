e53b2f6377dae267cd1bb158a842d46361f24b0c
Rakesh <rakeshkumar8900@gmail.com>
2018-12-26 17:38:42 +0530

    list tascomponent

--- src/Components/Task/Listing/Tasklisting.js
+++ src/Components/Task/Listing/Tasklisting.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 const Tasklisting = (props) =>{
-  
+   
+    const doneCmoponent = (id)=>{
+       return (
+           
+              <span class="badge badge-success"
+                    style={{cursor:"pointer",marginLeft:"10px"}}
+                    onClick={()=>props.doneHandler(id)}>
+                Mark Completed</span>
+            
+       )
+    }
+
     return (
         <ul className="list-group">
-            {props.tasks.map( task => <li className="list-group-item">{task.title}</li>  )}
+            {props.tasks.map( (task) =>{
+                return (
+                    <li style={task.done?{textDecoration:"line-through"}:null} 
+                        key={task.id} 
+                        className="list-group-item"                        
+                        >
+                        <span style={{wordBreak:"break-all"}}>{task.title}</span>                         
+                        <span onClick={()=>props.deleteHandler(task.id)} 
+                              className="float-right close">X</span>{' '}
+                              {!task.done && doneCmoponent(task.id)}
+                    </li>
+                )
+            })}
         </ul> 
     );
 }
