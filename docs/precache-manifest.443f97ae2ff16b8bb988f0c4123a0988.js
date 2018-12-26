c3e178c30cc0b9ce3ffec88dfc8478500e04b0b5
Rakesh <rakeshkumar8900@gmail.com>
2018-12-26 18:35:46 +0530

    production build

---
+++ docs/precache-manifest.443f97ae2ff16b8bb988f0c4123a0988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92d98714e9d19c32289b",
+    "url": "/static/js/main.92d98714.chunk.js"
+  },
+  {
+    "revision": "e9cd75d66ce1adffb545",
+    "url": "/static/js/1.e9cd75d6.chunk.js"
+  },
+  {
+    "revision": "92d98714e9d19c32289b",
+    "url": "/static/css/main.8521a9bd.chunk.css"
+  },
+  {
+    "revision": "9baa59977d47f15925ddb24a2a1594a9",
+    "url": "/index.html"
+  }
+];
