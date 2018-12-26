931cf5ddac63d81f7382a761fbfb5d030b0d1e51
Rakesh <rakeshkumar8900@gmail.com>
2018-12-26 17:54:05 +0530

    button allignment issue fix

--- src/Components/Task/Add/Addtask.js
+++ src/Components/Task/Add/Addtask.js
@@ -7,18 +7,14 @@ const Layout = (props) =>{
     return(        
         <div className="row">
             <div className="col-12">
-                <form onSubmit={ (e)=>{props.submitHandler(e)}} name="add-task">
-                <div className="input-group">                    
-                    <div className="input-group-append">
+                <form onSubmit={ (e)=>{props.submitHandler(e)}} name="add-task" className="form-inline">                                                      
                     <input type="text" 
                            name="addtask" 
-                           className="form-control col-11" 
+                           className="form-control mr-sm-2" 
                            onChange={props.updateHandler}
                            value={props.value}
-                           />  {' '}
-                        <button className="btn btn-primary" type="submit">Add</button>
-                    </div>
-                </div>
+                           />                         
+                        <button className="btn btn-outline-primary my-2 my-sm-0 col-3" type="submit">Add</button>                                            
                 </form>
            </div>
         </div>
