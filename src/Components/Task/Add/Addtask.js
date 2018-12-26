05cccef9e53fc4e56b389609a2ef435abfc7dff6
Rakesh <rakeshkumar8900@gmail.com>
2018-12-26 17:38:22 +0530

    add task form

--- src/Components/Task/Add/Addtask.js
+++ src/Components/Task/Add/Addtask.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 const Layout = (props) =>{
+    
+    
 
     return(        
         <div className="row">
             <div className="col-12">
-                <form onSubmit={props.submitHandler}>
+                <form onSubmit={ (e)=>{props.submitHandler(e)}} name="add-task">
                 <div className="input-group">                    
                     <div className="input-group-append">
-                    <input type="text" name="addtask" className="form-control col-11" />  
+                    <input type="text" 
+                           name="addtask" 
+                           className="form-control col-11" 
+                           onChange={props.updateHandler}
+                           value={props.value}
+                           />  {' '}
                         <button className="btn btn-primary" type="submit">Add</button>
                     </div>
                 </div>
