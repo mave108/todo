c69b1e5585daf940bcf3f073e337c23e201775e4
Rakesh <rakeshkumar8900@gmail.com>
2018-12-27 11:46:41 +0530

    New production build

---
+++ docs/precache-manifest.b80758134edf18b43a377fdf765f69c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "d2d05c9a0c49a13d29ad",
+    "url": "/todo/static/js/main.d2d05c9a.chunk.js"
+  },
+  {
+    "revision": "e9cd75d66ce1adffb545",
+    "url": "/todo/static/js/1.e9cd75d6.chunk.js"
+  },
+  {
+    "revision": "d2d05c9a0c49a13d29ad",
+    "url": "/todo/static/css/main.8521a9bd.chunk.css"
+  },
+  {
+    "revision": "233bee24c5d4852f8d84882b99145fa8",
+    "url": "/todo/index.html"
+  }
+];
